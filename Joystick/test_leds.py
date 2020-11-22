ca99c49e62224032274f16f82dd63df05804ea8a
Jeshwin Prince <jeshwinprince@Jeshwins-Air.attlocal.net>
2020-11-21 22:54:56 -0800

    add all files

---
+++ Joystick/test_leds.py
@@ -0,0 +1,24 @@
+from gpiozero import LED
+from time import sleep
+
+ledR = LED(13)
+ledG = LED(16)
+ledB = LED(26)
+
+def main:
+  while True:
+    led1.on()
+    sleep(10)
+    led1.off()
+    sleep(10)
+    led2.on()
+    sleep(10)
+    led2.off()
+    sleep(10)
+    led3.on()
+    sleep(10)
+    led3.off()
+    sleep(10)
+
+if name = "main":
+  main()
